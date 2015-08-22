31619dfdef8265ec155c90cb82fc575901b6a3e7
Rex Soriano <awesomerex@gmail.com>
2015-08-21 22:49:23 -0400

    draw player add movement

--- game.js
+++ game.js
@@ -16,6 +16,11 @@ var manifest = {
 
 var game = new Splat.Game(canvas, manifest);
 
+function drawPlayer(context, drawable){
+	context.fillStyle = drawable.color;
+	context.fillRect(drawable.x, drawable.y, drawable.height, drawable.width);
+}
+
 function centerText(context, text, offsetX, offsetY) {
 	var w = context.measureText(text).width;
 	var x = offsetX + (canvas.width / 2) - (w / 2) | 0;
@@ -25,8 +30,25 @@ function centerText(context, text, offsetX, offsetY) {
 
 game.scenes.add("title", new Splat.Scene(canvas, function() {
 	// initialization
+	var scene = this;
+	scene.player = new Splat.Entity(canvas.width/4, canvas.height - 50, 50, 50);
+	scene.player.color = "red";
+
 }, function() {
 	// simulation
+	if (game.keyboard.isPressed("left")) {
+		this.player.x -= 1;
+	}
+	if (game.keyboard.isPressed("right")) {
+		this.player.x += 1;
+	}
+	if (game.keyboard.isPressed("up")) {
+		this.player.y -= 1;
+	}
+	if (game.keyboard.isPressed("down")) {
+		this.player.y += 1;
+	}
+
 }, function(context) {
 	// draw
 	context.fillStyle = "#092227";
@@ -35,6 +57,9 @@ game.scenes.add("title", new Splat.Scene(canvas, function() {
 	context.fillStyle = "#fff";
 	context.font = "25px helvetica";
 	centerText(context, "Blank SplatJS Project", 0, canvas.height / 2 - 13);
+
+	drawPlayer(context, this.player);
+
 }));
 
 game.scenes.switchTo("loading");
