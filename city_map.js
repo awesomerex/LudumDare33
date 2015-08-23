6208d4525a83dda84d3dc10bc6f7b28a61d2dd1f
Lyonsclay <lyonsclay@yahoo.com>
2015-08-23 15:15:06 -0400

    Compile city_map

--- city_map.js
+++ city_map.js
@@ -24,6 +24,8 @@ function buildCity() {
                 }
 
                 var building = new generateBuilding(x * 32, y * 32, number, 0, height * 32); 
+            } else {
+                generateTile(x * 32, y * 32, tile);
             }
         }
     }
@@ -62,4 +64,49 @@ function generateBuilding(x, y, width, height, buildingNumber, offsetx, offsety)
 	};
 	return entity;
 }
+	game.spriteLookup = 
+	{
+	0: {"x":0, "y":0},
+	1: {"x":32, "y":0},
+	2: {"x":64, "y":0},
+	3: {"x":96, "y":0},
+	4: {"x":128, "y":0},
+	
+	5: {"x":0, 	 "y":32},
+	6: {"x":32,	 "y":32},
+	7: {"x":64,	 "y":32},
+	8: {"x":96,	 "y":32},
+	9: {"x":128, "y":32},
 
+	10: {"x":0,   "y":64},
+	11: {"x":32,  "y":64},
+	12: {"x":64,  "y":64},
+	13: {"x":96,  "y":64},
+	14: {"x":128, "y":64},
+
+	15: {"x":0,   "y":96},
+	16: {"x":32,  "y":96},
+	17: {"x":64,  "y":96},
+	18: {"x":96,  "y":96},
+	19: {"x":128, "y":96},
+
+	20: {"x":0,   "y":128},
+	21: {"x":32,  "y":128},
+	22: {"x":64,  "y":128},
+	23: {"x":96,  "y":128},
+	24: {"x":128, "y":128},
+
+	25: {"x":0,   "y":160},
+	26: {"x":32,  "y":160},
+	27: {"x":64,  "y":160},
+	28: {"x":96,  "y":160},
+	29: {"x":128, "y":160},
+};
+function generateTile(x, y, index){
+	var entity = new Splat.Entity(x, y, 32, 32);
+	entity.index = index;
+	entity.draw = function drawFromTileSet(context){
+		context.drawImage(game.tilesheet, game.spriteLookup[this.index].x, game.spriteLookup[this.index].y, 32, 32, this.x, this.y, 32, 32);
+	};
+	return entity;
+}
